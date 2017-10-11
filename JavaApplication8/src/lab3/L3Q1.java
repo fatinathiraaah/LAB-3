980175e6226ea58daad2a17ad584c40499a8fcd8
NUR FATIN ATHIRAH BINTI CHE ISMAIL <32027937+fatinathiraaah@users.noreply.github.com>
2017-10-11 15:27:02 +0800

    Update L3Q1.java

--- JavaApplication8/src/lab3/L3Q1.java
+++ JavaApplication8/src/lab3/L3Q1.java
@@ -1,23 +1,14 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package lab3;
 
 import java.util.Scanner;
 
-/**
- *
- * @author user
- */
+
 public class L3Q1 {
 
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+       
         Scanner s=new Scanner (System.in);
     double z;
     System.out.print("Enter two integer number:");
