d1b6ad443c54d6b1b6e045b0d24cc343d9915dc3
NUR FATIN ATHIRAH BINTI CHE ISMAIL <32027937+fatinathiraaah@users.noreply.github.com>
2017-10-11 15:41:46 +0800

    Update L3Q3.java

--- JavaApplication8/src/lab3/L3Q3.java
+++ JavaApplication8/src/lab3/L3Q3.java
@@ -1,23 +1,14 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package lab3;
 
 import java.util.Scanner;
 
-/**
- *
- * @author user
- */
+
 public class L3Q3 {
 
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+        
         Scanner s=new Scanner(System.in);
         double comm,sale;
         System.out.println("Enter the sales volume:");
