a51849ad4cfb73cb41c5202008ec83f69d96fc7f
NUR FATIN ATHIRAH BINTI CHE ISMAIL <32027937+fatinathiraaah@users.noreply.github.com>
2017-10-11 15:34:02 +0800

    Update L3Q1.java

--- JavaApplication8/src/lab3/L3Q1.java
+++ JavaApplication8/src/lab3/L3Q1.java
@@ -12,9 +12,9 @@ public class L3Q1 {
         Scanner s=new Scanner (System.in);
     double z;
     System.out.print("Enter two integer number:");
-    double x=s.nextInt();
-    double y=s.nextInt();
-    System.out.print("Enter the operand:");
+    double x=s.nextDouble();
+    double y=s.nextDouble();
+    System.out.print("Enter the operator:");
     char oper=s.next().charAt(0);{
     
             switch (oper) {
@@ -39,7 +39,7 @@ public class L3Q1 {
                     System.out.println(x+" "+oper+" "+y+" = "+z);
                     break;
                 default:
-                    System.out.println("enter another operand");
+                    System.out.println("enter another operator please :)");
                     break;
             }
     
